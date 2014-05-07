5ddb7010f55dbdda61505bdb41efc93b33160339
FelixBraig <felix.braig@googlemail.com>
2014-05-07 20:31:10 +0200

    Added storyboard mensa & VCMensa

---
+++ uni-hd/UHDMensaViewController.h
@@ -0,0 +1,13 @@
+//
+//  UHDMensaViewController.h
+//  uni-hd
+//
+//  Created by Felix on 07.05.14.
+//  Copyright (c) 2014 Universität Heidelberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UHDMensaViewController : UITableViewController
+
+@end
---
+++ uni-hd/UHDMensaViewController.m
@@ -0,0 +1,18 @@
+//
+//  UHDMensaViewController.m
+//  uni-hd
+//
+//  Created by Felix on 07.05.14.
+//  Copyright (c) 2014 Universität Heidelberg. All rights reserved.
+//
+
+#import "UHDMensaViewController.h"
+
+@interface UHDMensaViewController ()
+
+@end
+
+@implementation UHDMensaViewController
+
+
+@end
