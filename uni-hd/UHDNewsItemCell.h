b47e5d6009cc7794558faa423e255ae0a9724bef
Kevin Geier <Kevin.Geier91@Gmail.com>
2014-05-14 21:40:19 +0200

    added framework to manually configure UHDNewsItemCell

---
+++ uni-hd/UHDNewsItemCell.h
@@ -0,0 +1,17 @@
+//
+//  UHDNewsItemCell.h
+//  uni-hd
+//
+//  Created by Kevin Geier on 14.05.14.
+//  Copyright (c) 2014 Universität Heidelberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UHDNewsItemCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *abstractLabel;
+
+
+@end
---
+++ uni-hd/UHDNewsItemCell.m
@@ -0,0 +1,13 @@
+//
+//  UHDNewsItemCell.m
+//  uni-hd
+//
+//  Created by Kevin Geier on 14.05.14.
+//  Copyright (c) 2014 Universität Heidelberg. All rights reserved.
+//
+
+#import "UHDNewsItemCell.h"
+
+@implementation UHDNewsItemCell
+
+@end
