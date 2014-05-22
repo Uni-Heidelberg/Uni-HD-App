3fa71a169035bbf4314a774e03e8d04af5702e5c
Nils Fischer <n.fischer@viwid.com>
2014-05-22 21:27:38 +0200

    added brand color category

---
+++ uni-hd/UIColor+UHDBrandColor.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+UHDBrandColor.m
+//  uni-hd
+//
+//  Created by Nils Fischer on 22.05.14.
+//  Copyright (c) 2014 Universität Heidelberg. All rights reserved.
+//
+
+#import "UIColor+UHDBrandColor.h"
+
+@implementation UIColor (UHDBrandColor)
+
+@end
