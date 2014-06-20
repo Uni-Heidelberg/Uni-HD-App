70c8ede0b1b76fe6358eb07cd55fc65fca05ff9f
Nils Fischer <n.fischer@viwid.com>
2014-06-20 19:21:55 +0200

    implemented news source paging

---
+++ uni-hd/UHDNewsListViewController.m
@@ -0,0 +1,100 @@
+//
+//  UHDNewsListViewController.m
+//  uni-hd
+//
+//  Created by Nils Fischer on 20.06.14.
+//  Copyright (c) 2014 Universität Heidelberg. All rights reserved.
+//
+
+#import "UHDNewsListViewController.h"
+
+#import "VIFetchedResultsControllerDataSource.h"
+#import "UHDRemoteDatasourceManager.h"
+
+#import "UHDNewsDetailViewController.h"
+
+#import "UHDNewsItem.h"
+#import "UHDNewsSource.h"
+
+// Table View Cells
+#import "UHDNewsItemCell.h"
+#import "UHDNewsItemCell+ConfigureForItem.h"
+
+
+@interface UHDNewsListViewController ()
+
+@property (strong, nonatomic) VIFetchedResultsControllerDataSource *fetchedResultsControllerDataSource;
+
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)refreshControlValueChanged:(id)sender;
+
+@end
+
+
+@implementation UHDNewsListViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // redirect data source
+    self.tableView.dataSource = self.fetchedResultsControllerDataSource;
+}
+
+- (void)setSources:(NSArray *)sources
+{
+    _sources = sources;
+    self.managedObjectContext = [(NSManagedObject *)[sources lastObject] managedObjectContext];
+    if (sources.count == 1) {
+        self.title = [sources[0] title];
+    }
+}
+
+
+#pragma mark - User Interaction
+
+- (IBAction)refreshControlValueChanged:(UIRefreshControl *)sender
+{
+    [[[UHDRemoteDatasourceManager defaultManager] remoteDatasourceForKey:UHDRemoteDatasourceKeyNews] refreshWithCompletion:^(BOOL success, NSError *error) {
+        [sender endRefreshing];
+    }];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"showNewsDetail"]) {
+        
+        UHDNewsDetailViewController *newsDetailVC = segue.destinationViewController;
+        
+        // Mark item as read
+        UHDNewsItem *item = self.fetchedResultsControllerDataSource.selectedItem;
+        item.read = YES;
+        [item.managedObjectContext saveToPersistentStore:NULL];
+        
+        newsDetailVC.newsItem = item;
+    }
+}
+
+#pragma mark - Data Source
+
+- (VIFetchedResultsControllerDataSource *)fetchedResultsControllerDataSource
+{
+    if (!_fetchedResultsControllerDataSource)
+    {
+        NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[UHDNewsItem entityName]];
+        fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"date" ascending:NO]];
+        fetchRequest.predicate = [NSPredicate predicateWithFormat:@"source IN %@", self.sources];
+        
+        NSFetchedResultsController *fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:nil];
+        
+        VITableViewCellConfigureBlock configureCellBlock = ^(UITableViewCell *cell, id item) {
+            [(UHDNewsItemCell *)cell configureForItem:item];
+        };
+        
+        self.fetchedResultsControllerDataSource = [[VIFetchedResultsControllerDataSource alloc] initWithFetchedResultsController:fetchedResultsController tableView:self.tableView cellIdentifier:@"newsCell" configureCellBlock:configureCellBlock];
+    }
+    return _fetchedResultsControllerDataSource;
+}
+
+@end
