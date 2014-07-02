d34442ea7b0e49bb8a5e9b1df8770a9cdca84d88
Andreas Schachner <Andreas_Schachner@dhcp563-085.laptop-wlc.uni-heidelberg.de>
2014-07-02 17:09:34 +0200

    fixed detail news view

--- uni-hd/UHDNewsDetailViewController.m
+++ uni-hd/UHDNewsDetailViewController.m
@@ -13,21 +13,15 @@
 @interface UHDNewsDetailViewController ()
 
 @property (strong, nonatomic) IBOutlet UIWebView *newsWebView;
-@property (nonatomic) UITapGestureRecognizer *tap;
-@property BOOL scalesPageToFit;
-@property (nonatomic) NSArray *arrayOfActivityItems;
-@property (nonatomic) UIActivityViewController *activityVC;
-@property(nonatomic, assign, getter=isTranslucent) BOOL translucent;
 
-
--(IBAction)UIButton:(id)shareButton;
-
+- (IBAction)shareButtonPressed:(id)sender;
 
 @end
 
 @implementation UHDNewsDetailViewController
 
-- (void)viewWillAppear:(BOOL)animated {
+- (void)viewWillAppear:(BOOL)animated
+{
     [super viewWillAppear:animated];
     
     self.navigationItem.title = self.newsItem.title;
@@ -36,45 +30,14 @@
     NSURLRequest *requestObj = [NSURLRequest requestWithURL:self.newsItem.url];
     [self.newsWebView loadRequest:requestObj];
     _newsWebView.scalesPageToFit = YES;
-   
     
 }
 
-
--(IBAction)UIButton:(id)shareButton{
-    
-
-    
-    UIActivityViewController *activityVC = [[UIActivityViewController alloc]
-                                            initWithActivityItems:@[self.newsItem.title,   self.newsItem.url]applicationActivities:nil];
-    
-    [self.navigationController presentViewController:activityVC animated:YES completion:nil];
-    
-    
-}
-
-
-
-- (void) hideShowNavigation
+- (IBAction)shareButtonPressed:(id)sender
 {
-    [self.navigationController setNavigationBarHidden:!self.navigationController.navigationBarHidden];
-}
-
-
-
-- (void)viewDidLoad {
-    
-    
-    [super viewDidLoad];
-    
-    self.navigationController.navigationBar.translucent = YES;
-    
-    UITapGestureRecognizer *tap = [[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideShowNavigation)] initWithTarget:self.newsWebView action: Nil];
-    tap.numberOfTapsRequired = 1;
-    [self.newsWebView addGestureRecognizer:tap];
-    
-
-
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc]
+                                            initWithActivityItems:@[self.newsItem.title, self.newsItem.url] applicationActivities:nil];
+    [self.navigationController presentViewController:activityVC animated:YES completion:nil];
 }
 
 @end
