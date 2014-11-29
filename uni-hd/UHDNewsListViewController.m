b39eb9597d0077931f1f9e19d815a6ae76618459
Kevin Geier <Kevin.Geier91@Gmail.com>
2014-11-29 23:35:33 +0100

    first attempt to implement section headers in news list

--- uni-hd/UHDNewsListViewController.m
+++ uni-hd/UHDNewsListViewController.m
@@ -28,12 +28,20 @@
 
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 
+
+@property (strong, nonatomic) NSMutableDictionary *sectionsDictionary;
+@property (strong, nonatomic) NSArray *sectionsArray;
+@property (strong, nonatomic) NSDateFormatter *sectionDateFormatter;
+
+
 - (IBAction)refreshControlValueChanged:(id)sender;
+- (void)groupFetchedItemsByWeek;
 
 @end
 
 
 @implementation UHDNewsListViewController
+
 
 - (void)awakeFromNib {
     [super awakeFromNib];
@@ -41,12 +49,20 @@
     self.tableView.estimatedRowHeight = 200;
 }
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+	
+	// set Date Formatter
+	self.sectionDateFormatter = [[NSDateFormatter alloc] init];
+	[self.sectionDateFormatter setDateStyle:NSDateFormatterMediumStyle];
+	[self.sectionDateFormatter setTimeStyle:NSDateFormatterNoStyle];
+	
 	self.tableView.dataSource = self;
 	self.tableView.delegate = self;
 }
+
 
 - (void)setSources:(NSArray *)sources
 {
@@ -59,11 +75,17 @@
     self.fetchedResultsControllerDataSourceEvents.fetchedResultsController.fetchRequest.predicate = [NSPredicate predicateWithFormat:@"source IN %@", self.sources];
     [self.fetchedResultsControllerDataSourceNews reloadData];
     [self.fetchedResultsControllerDataSourceEvents reloadData];
+	
+	[self groupFetchedItemsByWeek];
 }
+
 
 - (void)setDisplayMode:(UHDNewsListDisplayMode)displayMode
 {
 	_displayMode = displayMode;
+	
+	[self groupFetchedItemsByWeek];
+	
     [self.tableView reloadData];
 }
 
@@ -75,6 +97,7 @@
     [[[UHDRemoteDatasourceManager defaultManager] remoteDatasourceForKey:UHDRemoteDatasourceKeyNews] refreshWithCompletion:^(BOOL success, NSError *error) {
         [sender endRefreshing];
     }];
+	// TODO: Refreshing throws an exception
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -82,10 +105,15 @@
     if ([segue.identifier isEqualToString:@"showNewsDetail"]) {
         
         UHDNewsDetailViewController *newsDetailVC = segue.destinationViewController;
-        
-        // Mark item as read
-        UHDNewsItem *item = self.fetchedResultsControllerDataSource.selectedItem;
-        item.read = YES;
+		
+		NSIndexPath *indexPath = self.tableView.indexPathForSelectedRow;
+		NSArray *items = [self.sectionsDictionary objectForKey:self.sectionsArray[indexPath.section]];
+		UHDNewsItem *item = items[indexPath.row];
+		
+		//UHDNewsItem *item = self.fetchedResultsControllerDataSource.selectedItem;
+		
+		// Mark item as read
+		item.read = YES;
         [item.managedObjectContext saveToPersistentStore:NULL];
         
         newsDetailVC.newsItem = item;
@@ -134,6 +162,25 @@
         }
         
         NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[UHDNewsItem entityName]];
+		
+		// Idea: write custom comparator for sorting items by calendar week
+		/*
+		NSComparator compareDatesByWeekOfYear = ^(id date1, id date2) {
+		
+			NSCalendar *calendar = [NSCalendar currentCalendar];
+			NSTimeZone *timeZone = [NSTimeZone systemTimeZone];
+			[calendar setTimeZone:timeZone];
+			
+			NSDateComponents *weekComponents1 = [calendar components: NSCalendarUnitYear | NSCalendarUnitWeekOfYear fromDate:date1];
+			NSDateComponents *weekComponents2 = [calendar components: NSCalendarUnitYear | NSCalendarUnitWeekOfYear fromDate:date2];
+			
+			NSDate *dateRepresentingDate1 = [calendar dateFromComponents:weekComponents1];
+			NSDate *dateRepresentingDate2 = [calendar dateFromComponents:weekComponents2];
+			
+			return [dateRepresentingDate1 compare:dateRepresentingDate2];
+
+		};
+		*/
 
         fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"date" ascending:NO]];
         
@@ -145,6 +192,12 @@
 		self.fetchedResultsControllerDataSourceNews = fetchedResultsControllerDataSource;
 		
     }
+	
+	/*
+	NSArray *sections = _fetchedResultsControllerDataSourceNews.fetchedResultsController.sections;
+	[self.logger log:[NSString stringWithFormat:@"number of sections: %lu", [sections count]] forLevel:VILogLevelDebug];
+	*/
+	
 	return _fetchedResultsControllerDataSourceNews;
 }
 
@@ -182,27 +235,36 @@
     return _fetchedResultsControllerDataSourceEvents;
 }
 
+
 #pragma mark - Table View Datasource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-	return [self.fetchedResultsControllerDataSource numberOfSectionsInTableView:tableView];
+	//return [self.fetchedResultsControllerDataSource numberOfSectionsInTableView:tableView];
+	
+	return [self.sectionsArray count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-	return [self.fetchedResultsControllerDataSource tableView:tableView numberOfRowsInSection:section];
+	//return [self.fetchedResultsControllerDataSource tableView:tableView numberOfRowsInSection:section];
+	
+	return [[self.sectionsDictionary objectForKey:self.sectionsArray[section]] count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	UITableViewCell *cell = nil;
+	
+	NSArray *items = [self.sectionsDictionary objectForKey:self.sectionsArray[indexPath.section]];
 
 	if (self.displayMode == UHDNewsListDisplayModeNews) {
 
         static NSString *newsCellIdentifier = @"newsCell";
 
-        UHDNewsItem *item = [self.fetchedResultsControllerDataSource.fetchedResultsController objectAtIndexPath:indexPath];
+        //UHDNewsItem *item = [self.fetchedResultsControllerDataSource.fetchedResultsController objectAtIndexPath:indexPath];
+		
+		UHDNewsItem *item = items[indexPath.row];
 	
         cell = [tableView dequeueReusableCellWithIdentifier:newsCellIdentifier forIndexPath:indexPath];
 		[(UHDNewsItemCell *)cell configureForItem:item];
@@ -210,7 +272,9 @@
 	}
 	else {
 	
-		UHDEventItem *item = [self.fetchedResultsControllerDataSource.fetchedResultsController objectAtIndexPath:indexPath];
+		//UHDEventItem *item = [self.fetchedResultsControllerDataSource.fetchedResultsController objectAtIndexPath:indexPath];
+		
+		UHDEventItem *item = items[indexPath.row];
 		
 		if ([[item entityName] isEqualToString:[UHDEventItem entityName]]) {
 			cell = [tableView dequeueReusableCellWithIdentifier:@"eventCell" forIndexPath:indexPath];
@@ -226,9 +290,107 @@
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-	return [self.fetchedResultsControllerDataSource tableView:tableView titleForHeaderInSection:section];
+	// Use user's current calendar and time zone
+	NSCalendar *calendar = [NSCalendar currentCalendar];
+	NSTimeZone *timeZone = [NSTimeZone systemTimeZone];
+	[calendar setTimeZone:timeZone];
+	
+	NSDate *startDate = self.sectionsArray[section];
+	
+	NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
+	[dateComponents setWeekOfYear:1];
+	[dateComponents setDay:-1];
+	
+	NSDate *endDate = [calendar dateByAddingComponents:dateComponents toDate:startDate options:0];
+	
+	NSString *startDateString = [self.sectionDateFormatter stringFromDate:startDate];
+	NSString *endDateString = [self.sectionDateFormatter stringFromDate:endDate];
+	
+	
+	NSString *sectionHeader = [NSString stringWithFormat:@"%@ - %@", startDateString, endDateString];
+	
+	return sectionHeader;
+	
+	//return [self.fetchedResultsControllerDataSource tableView:tableView titleForHeaderInSection:section];
 }
 
+
+# pragma mark - Table View Section Headers
+
+
+- (NSDate *)dateOfBeginningCalendarWeekForDate: (NSDate *)date {
+	
+	// Use user's current calendar and time zone
+	NSCalendar *calendar = [NSCalendar currentCalendar];
+	NSTimeZone *timeZone = [NSTimeZone systemTimeZone];
+	[calendar setTimeZone:timeZone];
+	
+	NSDateComponents *dateComponents = [calendar components:NSCalendarUnitYear | NSCalendarUnitYearForWeekOfYear | NSCalendarUnitWeekOfYear fromDate:date];
+	
+	NSDate *beginningOfCalendarWeek = [calendar dateFromComponents:dateComponents];
+	
+	// Add one day to let week start on Monday: can cause trouble when item's date is Sunday
+	/*
+	NSDateComponents *dayComponent = [[NSDateComponents alloc] init];
+	[dayComponent setDay:1];
+	beginningOfCalendarWeek = [calendar dateByAddingComponents:dayComponent toDate:beginningOfCalendarWeek options:0];
+	*/
+	
+	
+	/*
+	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+	[dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+	[dateFormatter setTimeStyle:NSDateFormatterShortStyle];
+	[self.logger log:[NSString stringWithFormat:@"test date: %@", [dateFormatter stringFromDate:beginningOfCalendarWeek]] forLevel:VILogLevelDebug];
+	*/
+	
+	return beginningOfCalendarWeek;
+}
+
+
+- (void)groupFetchedItemsByWeek {
+
+	self.sectionsDictionary = [[NSMutableDictionary alloc] init];
+	
+	// Use user's current calendar and time zone
+	NSCalendar *calendar = [NSCalendar currentCalendar];
+	NSTimeZone *timeZone = [NSTimeZone systemTimeZone];
+	[calendar setTimeZone:timeZone];
+	
+	/*
+	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+	[dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+	[dateFormatter setTimeStyle:NSDateFormatterShortStyle];
+	*/
+	
+	for (UHDNewsItem *item in self.fetchedResultsControllerDataSource.fetchedResultsController.fetchedObjects) {
+	
+		//[self.logger log:[NSString stringWithFormat:@"date of news item: %@", [dateFormatter stringFromDate:item.date]] forLevel:VILogLevelDebug];
+		
+		NSDate *week = [self dateOfBeginningCalendarWeekForDate:item.date];
+		
+		//[self.logger log:[NSString stringWithFormat:@"week of news item: %@", [dateFormatter stringFromDate:week]] forLevel:VILogLevelDebug];
+		
+		NSMutableArray *items = [self.sectionsDictionary objectForKey:week];
+		if (items == nil) {
+			items = [NSMutableArray arrayWithObject:item];
+			[self.sectionsDictionary setObject:items forKey:week];
+		}
+		else {
+			[[self.sectionsDictionary objectForKey:week] addObject:item];
+		}
+		
+	};
+	
+	NSArray *weeks = [self.sectionsDictionary allKeys];
+	
+	NSArray *sortDescriptors = @[ [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO] ];
+	
+	self.sectionsArray = [weeks sortedArrayUsingDescriptors:sortDescriptors];
+	
+	//[self.logger log:[NSString stringWithFormat:@"number of weeks: %lu", [weeks count]] forLevel:VILogLevelDebug];
+
+}
 
 
 @end
